0dfc3681057efb86d880c6973d9aa06a89ffc0ed
Stan <stanvanmeerendonk@gmail.com>
2020-03-31 12:16:07 +0200

    Make example a bit clearer

--- src/main/java/com/uu/app/App.java
+++ src/main/java/com/uu/app/App.java
@@ -13,24 +13,28 @@ public class App {
 
 		GatlParserHandler handler = new GatlParserHandler();
 
-		handler.ParseAndPrint("reify(pi(sigma( interpol(noise,locations)  , euros)) )");
+		handler.ParseAndPrint("reify(pi(sigma( interpol(noise,locations),euros)) )");
 
 		SLTLBuilder test = new SLTLBuilder("true")
 			.addNext("interpol")
 			.addNext("sigma")
 			.addUnary(UnarySLTLOp.Future);
 
-		SLTL kaas = new SLTLBuilder("test2")
+		SLTL finalSltl = new SLTLBuilder("true")
 			.AddBinaryRight(test, BinarySLTLOp.Until).getResult();
 
-		SLTLFoldData<Expression<StateData>> test2 = SLTLFoldData.CreateStateFold(3);
+		//SLTLFoldData<Expression<StateData>> test2 = SLTLFoldData.CreateStateFold(2);
 
-		Expression<StateData> res = kaas.Fold(test2);
+		Expression<StateData> res = finalSltl.StateFold(2);
 		// f <sigma> <interpol> true yields ((sigma(s1) & interpol(s2) & true(s2)) | sigma(s2)
 
+		System.out.println("formula");
+		System.out.println(finalSltl);
+		System.out.println("Propositional");
 		System.out.println(res);
-		res = RuleSet.toCNF(res);
 
+		res = RuleSet.toCNF(res);
+		System.out.println("Cnf");
 		System.out.println(res);
 
 		System.out.println("Hello World!");
