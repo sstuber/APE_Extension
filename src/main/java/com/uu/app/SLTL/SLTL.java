be5559700c1966cfcaf7d7086c49215842134454
Stan <stanvanmeerendonk@gmail.com>
2020-03-31 12:15:03 +0200

    Decrease complexity of executing a fold by creating a function on the SLTL abstract class

--- src/main/java/com/uu/app/SLTL/SLTL.java
+++ src/main/java/com/uu/app/SLTL/SLTL.java
@@ -1,5 +1,14 @@
 package com.uu.app.SLTL;
 
+import com.bpodgursky.jbool_expressions.Expression;
+import com.uu.app.SLTL.StateFold.StateData;
+
 public abstract class SLTL {
 	public abstract <T> T Fold(SLTLFoldData<T> foldData);
+
+	public Expression<StateData> StateFold(int maxBound) {
+		SLTLFoldData<Expression<StateData>> test = SLTLFoldData.CreateStateFold(maxBound);
+		return this.Fold(test);
+	}
+
 }
